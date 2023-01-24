5108451bc1e3ecd8d03fe6d75bfdfc271c9e299e
VetaShine <111084369+VetaShine@users.noreply.github.com>
2023-01-24 16:57:34 +0300

    Create first_realization.c

---
+++ OS/lab№5/first_realization.c
@@ -0,0 +1,30 @@
+#include "realization.h"
+#include <stdlib.h> 
+#include <math.h>
+
+float derivative(float point, float increment) { 
+  return (cosf(point + increment) - cosf(point)) / increment;
+}
+
+char* translation(long numeric) {
+  char* result = (char*)malloc(sizeof(char)); 
+  int index = 0;
+  
+  while(numeric > 0) {
+    result[index] = numeric % 2 + '0';
+    index += 1;
+    numeric /= 2;
+    result = realloc(result, (index + 1) * sizeof(char));
+  }
+  
+  result[index] = '\0';
+  char symbol;
+
+  for(int permutation_index = 0; permutation_index < index / 2; ++permutation_index) {
+    symbol = result[permutation_index];
+    result[permutation_index] = result[index - permutation_index - 1];
+    result[index - permutation_index - 1] = symbol; 
+  }
+  
+  return result; 
+}
