f30ddfacedad17c7ca092131386405a8351a5566
VetaShine <111084369+VetaShine@users.noreply.github.com>
2023-01-24 14:23:51 +0300

    Update lab261.h

--- Workshop on Computer/lab№26/dynamic structure/lab261.h
+++ Workshop on Computer/lab№26/dynamic structure/lab261.h
@@ -1,12 +1,12 @@
 #ifndef LAB261_H
 #define LAB261_H
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define N 100
 #define Tvalue int
 
 struct El_St{ Tvalue V; El_St *next;};
-struct Stack{ El_St *first;int size;};
+struct Stack{ El_St *first; int size;};
 
 void Init(Stack &S);
 int Empty(Stack S);
